9ac8b92495fbf476a193e3113a0429f2495ecccb
whitzy <54811854+itswhitzy@users.noreply.github.com>
2022-11-26 19:13:56 +0300

    Update mover.cs

--- Assets/scripts/mover.cs
+++ Assets/scripts/mover.cs
@@ -7,7 +7,7 @@ public class mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("sex")
+        print("sex");
 
     // Update is called once per frame
     void Update()
